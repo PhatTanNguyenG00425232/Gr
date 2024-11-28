357be839412f70f2ba13a8e0ddf713e2a59b342a
PhatTanNguyenG00425232 <G00425232@atu.ie>
2024-11-28 21:03:06 +0000

    Plotting images with Python and Matplotlib

--- LAB_8.py
+++ LAB_8.py
@@ -5,5 +5,21 @@ from matplotlib import pyplot as plt
 # Load the image
 img = cv2.imread('ATU.jpg')
 
+# Convert the image to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Convert BGR image to RGB for correct color display with matplotlib
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+nrows = 2
+ncols = 1
+# Plot the original and grayscale images
+plt.subplot(nrows, ncols, 1), plt.imshow(img_rgb)
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(nrows, ncols, 2), plt.imshow(gray, cmap='gray')
+plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
 
 
+plt.tight_layout()
+plt.show()
